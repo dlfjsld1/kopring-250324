50a5b6958d0569b3904343d0f3f2e32aacdf7d48
dlfjsld1 <ltk900216@gmail.com>
2025-03-25 14:07:44 +0900

    postDto, securityUser

--- backend/src/main/java/com/example/upload/global/entity/BaseEntity.kt
+++ backend/src/main/java/com/example/upload/global/entity/BaseEntity.kt
@@ -1,6 +1,5 @@
-package com.example.upload.global.entity;
+package com.example.upload.global.entity
 
-import com.example.upload.domain.base.genFile.genFile.entity.GenFile
 import com.example.upload.standard.util.Ut
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -9,21 +8,26 @@ import jakarta.persistence.MappedSuperclass
 
 @MappedSuperclass
 abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null // TODO: 추후에 코틀린 전환 과정에서 해결
 
-    @Id // PRIMARY KEY
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
-    var id: Long? = null
+
 
     val modelName: String
         get() = Ut.str.lcfirst(this::class.simpleName)
 
     override fun equals(other: Any?): Boolean {
+
         if (other == null || javaClass != other.javaClass) return false
 
-        return id == (other as GenFile).id
+        other as BaseEntity
+
+        return id == other.id
     }
 
     override fun hashCode(): Int {
         return id?.hashCode() ?: System.identityHashCode(this)
     }
+
 }
